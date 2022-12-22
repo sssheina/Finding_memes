6d8b891f925c6ff6cbc6fd623d560230b9c293df
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2022-12-22 23:36:21 +0300

    Add files via upload

---
+++ assets/scripts/index.js
@@ -0,0 +1,39 @@
+let button = document.querySelector("button");
+let search = document.querySelector(".userSearch");
+let number = document.querySelector(".number");
+let gifs = document.querySelector(".pic");
+
+button.addEventListener("click", function () {
+    
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=AUR1oawy4r9z1QfA3jhCx7utLuYnNup9&q=${search.value}&limit=${number.value}&offset=0&rating=g&lang=en`
+  )
+    .then((res) => {
+      try {if (window.navigator.online == false)
+        throw new Error ("Check Internet Connection...");
+      } catch (error) {
+        alert("Web error: " + error.message);
+      }
+      try {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Check the validity of the entered request");
+      } catch (error) {
+        alert("Error: Invalid request " + error.message);
+      }
+    })
+    .then((elem) => {
+      console.log(elem);
+
+      for (let i = 0; i < elem.data.length; i++) {
+        gifs.innerHTML += `<div><img src="${elem.data[i].images.downsized.url}"></div>`;
+      }
+
+    });
+    search.value = '';
+    number.value = '';
+},
+
+
+);
