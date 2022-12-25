57b8eb83f50b0250a79ef77bbad4371f2b919611
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2022-12-25 05:06:10 +0300

    Add files via upload

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,13 +4,14 @@ let number = document.querySelector(".number");
 let gifs = document.querySelector(".pic");
 
 button.addEventListener("click", function () {
-    
   fetch(
     `https://api.giphy.com/v1/gifs/search?api_key=AUR1oawy4r9z1QfA3jhCx7utLuYnNup9&q=${search.value}&limit=${number.value}&offset=0&rating=g&lang=en`
+    // `https://oebhgeiptu.com`
   )
     .then((res) => {
-      try {if (window.navigator.online == false)
-        throw new Error ("Check Internet Connection...");
+      try {
+        if (window.navigator.online == false)
+          throw new Error("Check Internet Connection...");
       } catch (error) {
         alert("Web error: " + error.message);
       }
@@ -23,17 +24,20 @@ button.addEventListener("click", function () {
         alert("Error: Invalid request " + error.message);
       }
     })
+
     .then((elem) => {
       console.log(elem);
+
+      gifs.innerHTML = "";
 
       for (let i = 0; i < elem.data.length; i++) {
         gifs.innerHTML += `<div><img src="${elem.data[i].images.downsized.url}"></div>`;
       }
+    })
 
+    .catch((error) => {
+      alert("Web error: " + error.message);
     });
-    search.value = '';
-    number.value = '';
-},
-
-
-);
+  search.value = "";
+  number.value = "";
+});
